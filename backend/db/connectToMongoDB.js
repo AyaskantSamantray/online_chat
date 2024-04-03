eee243607fba8b5d087b3d1a87b14a02fa3bbb4c
Ayaskant <ayaskantsamantaray99@gmail.com>
2024-04-03 18:19:30 +0530

    thirdcommit

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect("mongodb://localhost:27017/whatsapp");
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ayaskant:1111@cluster0.5lvrzxm.mongodb.net/"
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
 };
 
-export default connectToMongoDB
+export default connectToMongoDB;
